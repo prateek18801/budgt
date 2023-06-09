ae82d66cc0a4af561435587164b8521e79e1e192
Prateek Chaurasia <prateek18801@gmail.com>
2023-06-09 21:47:59 +0530

    Setup temporary routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react';
+import AddExpense from './pages/AddExpense';
+import ExpenseAnalysis from './pages/ExpenseAnalysis';
+import ViewModifyExpense from './pages/ViewModifyExpense';
+import BottomNav from './components/BottomNav';
 import './App.css';
 
-function App() {
+const App = () => {
+
+    const [page, setPage] = useState(0);
+
     return (
-        <>Hello World</>
+        <div className='vh-100 vw-100 d-flex flex-column justify-content-between'>
+            {page == 0 ? <AddExpense /> : page === 1 ? <ViewModifyExpense /> : <ExpenseAnalysis />}
+            <BottomNav page={page} setPage={setPage} />
+        </div>
     );
 }
 
