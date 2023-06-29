271d9d4796e3735570884ad8eaba25a2def7aad7
Prateek Chaurasia <prateek18801@gmail.com>
2023-06-29 16:08:32 +0530

    Add form state and value attributes

--- src/pages/AddExpense.tsx
+++ src/pages/AddExpense.tsx
@@ -1,41 +1,58 @@
+import { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+
 const AddExpense: React.FC = () => {
+
+    const [formData, setFormData] = useState({
+        id: uuid(),
+        title: '',
+        amount: '',
+        category: 'food',
+        date: new Date().toISOString().substring(0, 10),
+        notes: ''
+    });
+
     return (
         <div className='m-3'>
             <h3 className='mb-4'>Add Expense</h3>
             <Form>
                 <Form.Group className='mb-3'>
                     <Form.Label>Title</Form.Label>
-                    <Form.Control type='text' required />
+                    <Form.Control type='text' name='title' value={formData.title} onChange={handleInputChange} />
                     <Form.Text className='text-muted'>100 out of 100 characters left</Form.Text>
                 </Form.Group>
+
                 <Form.Group className='mb-3'>
-                    <Form.Label>Amount</Form.Label>
-                    <Form.Control type='number' defaultValue={0} required />
+                    <Form.Label>Amount (₹)</Form.Label>
+                    <Form.Control type='number' name='amount' value={formData.amount} onChange={handleInputChange} />
                 </Form.Group>
+
                 <Form.Group className='mb-3'>
                     <Form.Label>Category</Form.Label>
-                    <Form.Select>
-                        <option>Food</option>
-                        <option>Travel</option> 
-                        <option>Groceries</option>
-                        <option>Bills</option>
-                        <option>Rent</option>
-                        <option>Health</option>
-                        <option>Others</option>
+                    <Form.Select name='category' value={formData.category} onChange={handleInputChange}>
+                        <option value='food'>Food</option>
+                        <option value='travel'>Travel</option>
+                        <option value='groceries'>Groceries</option>
+                        <option value='bills'>Bills</option>
+                        <option value='rent'>Rent</option>
+                        <option value='health'>Health</option>
+                        <option value='others'>Others</option>
                     </Form.Select>
                 </Form.Group>
+
                 <Form.Group className='mb-3'>
                     <Form.Label>Date</Form.Label>
-                    <Form.Control type='date' defaultValue={new Date().toISOString().substring(0, 10)} />
+                    <Form.Control type='date' name='date' value={formData.date} onChange={handleInputChange} />
                 </Form.Group>
+
                 <Form.Group className='mb-3'>
                     <Form.Label>Notes (Optional)</Form.Label>
-                    <Form.Control as='textarea' rows={3} />
+                    <Form.Control as='textarea' name='notes' rows={3} value={formData.notes} onChange={handleInputChange} />
                 </Form.Group>
-                <Button variant='primary' className='mt-3'>Add</Button>
+
+                <Button variant='primary' className='mt-3' onClick={handleSubmit}>Add</Button>
             </Form>
         </div>
     );
