a1212bf51af62f7e3d15c155ae6d2779404e5242
Prateek Chaurasia <prateek18801@gmail.com>
2023-06-30 17:59:42 +0530

    Update navigation add routes

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,21 +1,19 @@
-import { Dispatch, SetStateAction } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
-type props = {
-    page: number,
-    setPage: Dispatch<SetStateAction<number>>
-};
+const BottomNav: React.FC = () => {
 
-const BottomNav: React.FC<props> = ({ page, setPage }) => {
+    const navigate = useNavigate();
+
     return (
         <Container className='text-center bg-light py-3'>
             <Row>
-                <Col><Button variant={page === 1 ? 'success' : 'outline-success'} onClick={() => { setPage(1) }}>Edit</Button></Col>
-                <Col><Button variant={page === 0 ? 'success' : 'outline-success'} onClick={() => { setPage(0) }}>Add</Button></Col>
-                <Col><Button variant={page === 2 ? 'success' : 'outline-success'} onClick={() => { setPage(2) }}>Stats</Button></Col>
+                <Col><Button onClick={() => { navigate('/view') }}>View</Button></Col>
+                <Col><Button onClick={() => { navigate('/') }}>Add</Button></Col>
+                <Col><Button onClick={() => { navigate('/stats') }}>Stats</Button></Col>
             </Row>
         </Container>
     );
