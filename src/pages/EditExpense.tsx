b99fac567de196c268845d91a3e769c6059cfd8b
Prateek Chaurasia <prateek18801@gmail.com>
2023-06-30 18:51:19 +0530

    Implement update logic navigate back Set form state data type

--- src/pages/EditExpense.tsx
+++ src/pages/EditExpense.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import ExpenseForm from '../components/ExpenseForm';
 import useLocalStorage from '../hooks/useLocalStorage';
+
+type formDataType = {
+    id: string,
+    title: string,
+    amount: string,
+    category: string,
+    date: string,
+    notes: string
+}
 
 const EditExpense: React.FC = () => {
 
     const [expenses, setExpenses] = useLocalStorage('EXPENSES', []);
 
     const location = useLocation();
+    const navigate = useNavigate();
 
-    const [formData, setFormData] = useState(location.state);
+    const [formData, setFormData] = useState<formDataType>(location.state);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
         setFormData({
@@ -20,8 +30,8 @@ const EditExpense: React.FC = () => {
 
     const handleSubmit = () => {
         if (!(formData.title && formData.amount)) return;
-
-        setExpenses(prev => [formData, ...prev]);
+        setExpenses(prev => prev.map(obj => obj.id === formData.id ? formData : obj));
+        navigate(-1);
     }
 
 
