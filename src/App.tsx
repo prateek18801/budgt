4317fdae8897b3614f2f285ad8cddf9f02493357
Prateek Chaurasia <prateek18801@gmail.com>
2023-06-30 17:58:48 +0530

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import AddExpense from './pages/AddExpense';
 import ExpenseAnalysis from './pages/ExpenseAnalysis';
 import ViewModifyExpense from './pages/ViewModifyExpense';
+import EditExpense from './pages/EditExpense';
 import BottomNav from './components/BottomNav';
 import './App.css';
 
 const App = () => {
-
-    const [page, setPage] = useState(0);
-
     return (
         <div className='vh-100 vw-100 d-flex flex-column justify-content-between'>
-            {page == 0 ? <AddExpense /> : page === 1 ? <ViewModifyExpense /> : <ExpenseAnalysis />}
-            <BottomNav page={page} setPage={setPage} />
+            <Routes>
+                <Route path='/' element={<AddExpense />} />
+                <Route path='/view' element={<ViewModifyExpense />} />
+                <Route path='/edit' element={<EditExpense />} />
+                <Route path='/stats' element={<ExpenseAnalysis />} />
+            </Routes>
+            <BottomNav />
         </div>
     );
 }
