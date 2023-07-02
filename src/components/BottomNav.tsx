1e09d93c6ec7f604eca6f54263c6cf37570919f7
Prateek Chaurasia <prateek18801@gmail.com>
2023-07-02 19:14:44 +0530

    Remove container wrapper

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
@@ -9,13 +8,11 @@ const BottomNav: React.FC = () => {
     const navigate = useNavigate();
 
     return (
-        <Container className='text-center bg-light py-3'>
-            <Row>
-                <Col><Button onClick={() => { navigate('/view') }}>View</Button></Col>
-                <Col><Button onClick={() => { navigate('/') }}>Add</Button></Col>
-                <Col><Button onClick={() => { navigate('/stats') }}>Stats</Button></Col>
-            </Row>
-        </Container>
+        <Row className='text-center bg-white py-4 align-items-center'>
+            <Col><Button onClick={() => { navigate('/view') }}>View</Button></Col>
+            <Col><Button onClick={() => { navigate('/') }}>Add</Button></Col>
+            <Col><Button onClick={() => { navigate('/stats') }}>Stats</Button></Col>
+        </Row>
     );
 }
 
