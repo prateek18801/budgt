d749c7e2613e2fde7373398e28b432f3844531d5
Prateek Chaurasia <prateek18801@gmail.com>
2023-07-02 19:15:04 +0530

    Fix positioning and scroll issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import ExpenseAnalysis from './pages/ExpenseAnalysis';
 import ViewModifyExpense from './pages/ViewModifyExpense';
 import EditExpense from './pages/EditExpense';
 import BottomNav from './components/BottomNav';
+import TopNav from './components/TopNav';
 import './App.css';
 
 const App = () => {
     return (
-        <div className='vh-100 vw-100 d-flex flex-column justify-content-between'>
-            <Routes>
-                <Route path='/' element={<AddExpense />} />
-                <Route path='/view' element={<ViewModifyExpense />} />
-                <Route path='/edit' element={<EditExpense />} />
-                <Route path='/stats' element={<ExpenseAnalysis />} />
-            </Routes>
+        <div className='app__container'>
+            <TopNav />
+            <div className="overflow-y-scroll">
+                <Routes>
+                    <Route path='/' element={<AddExpense />} />
+                    <Route path='/view' element={<ViewModifyExpense />} />
+                    <Route path='/edit' element={<EditExpense />} />
+                    <Route path='/stats' element={<ExpenseAnalysis />} />
+                </Routes>
+            </div>
             <BottomNav />
         </div>
     );
