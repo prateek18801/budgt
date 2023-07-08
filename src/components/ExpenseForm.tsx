0c94c1fa900c1c84e749a573821ceec74492b960
Prateek Chaurasia <prateek18801@gmail.com>
2023-07-09 02:49:48 +0530

    Implement handleChange import formdata characters left in title

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,34 +1,49 @@
+import { useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-type props = {
+type formDataType = {
+    id: string,
     title: string,
     amount: string,
     category: string,
     date: string,
-    notes: string,
-    handleInputChange: (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => void,
+    notes: string
+}
+
+type props = {
+    formData: formDataType
+    setFormData: React.Dispatch<React.SetStateAction<formDataType>>,
     handleSubmit: () => void
 };
 
-const ExpenseForm: React.FC<props> = ({ title, amount, category, date, notes, handleInputChange, handleSubmit }) => {
+const ExpenseForm: React.FC<props> = ({ formData, setFormData, handleSubmit }) => {
+
+    const titleRef = useRef<HTMLInputElement>();
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
+        setFormData({
+            ...formData,
+            [e.currentTarget.name]: e.currentTarget.value
+        });
+    }
 
     return (
         <Form>
             <Form.Group className='mb-3'>
                 <Form.Label>Title</Form.Label>
-                <Form.Control type='text' name='title' value={title} onChange={handleInputChange} />
-                <Form.Text className='text-muted'>100 out of 100 characters left</Form.Text>
+                <Form.Control type='text' name='title' value={formData.title} ref={titleRef} onChange={handleInputChange} />
+                <Form.Text className='text-muted'>{50 - (titleRef.current?.value.length || 0)} out of 50 characters left</Form.Text>
             </Form.Group>
 
             <Form.Group className='mb-3'>
                 <Form.Label>Amount (₹)</Form.Label>
-                <Form.Control type='number' name='amount' value={amount} onChange={handleInputChange} />
+                <Form.Control type='number' name='amount' value={formData.amount} onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className='mb-3'>
                 <Form.Label>Category</Form.Label>
-                <Form.Select name='category' value={category} onChange={handleInputChange}>
+                <Form.Select name='category' value={formData.category} onChange={handleInputChange}>
                     <option value='food'>Food</option>
                     <option value='travel'>Travel</option>
                     <option value='groceries'>Groceries</option>
@@ -44,15 +59,15 @@ const ExpenseForm: React.FC<props> = ({ title, amount, category, date, notes, ha
 
             <Form.Group className='mb-3'>
                 <Form.Label>Date</Form.Label>
-                <Form.Control type='date' name='date' value={date} onChange={handleInputChange} />
+                <Form.Control type='date' name='date' value={formData.date} onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className='mb-3'>
                 <Form.Label>Notes (Optional)</Form.Label>
-                <Form.Control as='textarea' name='notes' rows={3} value={notes} onChange={handleInputChange} />
+                <Form.Control as='textarea' name='notes' rows={3} value={formData.notes} onChange={handleInputChange} />
             </Form.Group>
 
-            <Button variant='primary' className='mt-3' onClick={handleSubmit}>Add</Button>
+            <Button variant='primary' className='mt-3 d-flex align-items-center' onClick={handleSubmit}>Save</Button>
         </Form>
     );
 }
