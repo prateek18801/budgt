a0d5923491b392b46def0080e61842bff8270c80
Prateek Chaurasia <prateek18801@gmail.com>
2023-07-09 11:15:50 +0530

    Move charts to separate components

--- src/pages/ExpenseAnalysis.tsx
+++ src/pages/ExpenseAnalysis.tsx
@@ -1,22 +1,12 @@
-import { PieChart, Pie, Cell, LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import { useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import PieChart from '../components/PieChart';
+import LineChart from '../components/LineChart';
 import useLocalStorage from '../hooks/useLocalStorage';
-import { useState } from 'react';
-
-const CHART_COLORS = {
-    food: '#A52A2A',
-    travel: '#00BFFF',
-    groceries: '#7FFF00',
-    bills: '#808080',
-    rent: '#FF0000',
-    health: '#FFFF00',
-    investment: '#800080',
-    clothing: '#000000',
-    loan: '#FFA500',
-    others: '#FF4500'
-};
 
 const ExpenseAnalysis: React.FC = () => {
 
@@ -39,32 +29,18 @@ const ExpenseAnalysis: React.FC = () => {
             // distribute by date for line chart
             if (expense.date in dateAmountSum) dateAmountSum[expense.date] += (+expense.amount);
             else dateAmountSum[expense.date] = +expense.amount;
-
         }
         months.add(expense.date.split('-')[1]);
         years.add(expense.date.split('-')[0]);
     });
 
-    const piChartData = Object.keys(categoryAmountSum).map(key => ({ category: key, amount: categoryAmountSum[key] }));
+    const pieChartData = Object.keys(categoryAmountSum).map(key => ({ category: key, amount: categoryAmountSum[key] }));
     const lineChartData = Object.keys(dateAmountSum).sort().map(key => ({ date: key, amount: dateAmountSum[key] }));
-
-    // render pi chart labels
-    const RADIAN = Math.PI / 180;
-    const renderPiChartLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
-        const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-        const x = cx + radius * Math.cos(-midAngle * RADIAN);
-        const y = cy + radius * Math.sin(-midAngle * RADIAN);
-        return (
-            <text x={x} y={y} fill='white' textAnchor={x > cx ? 'start' : 'end'} dominantBaseline='central' style={{ fontSize: '10px' }}>
-                {`${piChartData[index].category} ${(percent * 100).toFixed(0)}%`}
-            </text>
-        );
-    };
 
     return (
         <div className='m-3'>
             <h3 className='mb-4'>Spend Analysis</h3>
-            <Form.Group className='mb-4'>
+            <Form.Group className='mb-3'>
                 <Row>
                     <Col>
                         <Form.Select onChange={(e) => { setMonth(e.target.value) }}>
@@ -82,40 +58,19 @@ const ExpenseAnalysis: React.FC = () => {
                     </Col>
                 </Row>
             </Form.Group>
-            
+
+            <Card className='mb-4'>
+                <Card.Body className='d-flex flex-column'>
+                    <Card.Title>₹ 6490</Card.Title>
+                    <Button variant='link'>More Details</Button>
+                </Card.Body>
+            </Card>
+
             <h6 className='text-center'>Monthly category wise analysis</h6>
-            <PieChart
-                width={350}
-                height={350}
-                style={{ left: '50%', transform: 'translateX(-50%)' }}
-            >
-                <Pie
-                    data={piChartData}
-                    dataKey='amount'
-                    outerRadius={140}
-                    label={renderPiChartLabel}
-                    labelLine={false}
-                >
-                    {piChartData.map((category, index) => (
-                        <Cell key={index} fill={CHART_COLORS[category.category]} />
-                    ))}
-                </Pie>
-            </PieChart>
+            <PieChart pieChartData={pieChartData} />
 
             <h6 className='mb-5 text-center'>Monthly day wise analysis</h6>
-            <LineChart
-                width={350}
-                height={300}
-                data={lineChartData}
-                style={{ left: '50%', transform: 'translateX(-50%)', fontSize: '12px' }}
-                margin={{ left: -20, right: 20 }}
-            >
-                <CartesianGrid strokeDasharray='3 3' />
-                <XAxis dataKey='date' />
-                <YAxis />
-                <Tooltip />
-                <Line type='monotone' dataKey='amount' stroke='#8884d8' activeDot={{ r: 6 }} />
-            </LineChart>
+            <LineChart lineChartData={lineChartData} />
         </div>
     );
 }
