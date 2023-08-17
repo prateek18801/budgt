abb2ac3b22857c0d20677755febc25de4ee10c05
Prateek Chaurasia <prateek18801@gmail.com>
2023-08-17 12:17:25 +0530

    Implement clear data

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import useLocalStorage from '../hooks/useLocalStorage';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 type formDataType = {
     budget: string
@@ -10,9 +12,13 @@ type formDataType = {
 
 const Settings: React.FC = () => {
 
+    const navigate = useNavigate();
+    const [expenses, setExpenses] = useLocalStorage('EXPENSES', []);
     const [config, setConfig] = useLocalStorage('CASHTRACK_CONFIG', { budget: '10000' });
     const [formData, setFormData] = useState<formDataType>(config);
-    const navigate = useNavigate();
+
+    const [fromDate, setFromDate] = useState<string>(`${new Date().getFullYear()}-${('0' + (new Date().getMonth() + 1)).slice(-2)}-01`);
+    const [toDate, setToDate] = useState<string>(`${new Date().getFullYear()}-${('0' + (new Date().getMonth() + 1)).slice(-2)}-${('0' + new Date().getDate()).slice(-2)}`);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>) => {
         setFormData({
@@ -23,7 +29,16 @@ const Settings: React.FC = () => {
 
     const handleSubmit = () => {
         setConfig(formData);
-        navigate(-1);
+    }
+
+    const clearData = () => {
+        if (confirm('This action cannot be undone!')) {
+            setExpenses(expenses =>
+                expenses.filter(expense =>
+                    expense.date < fromDate && expense.date > toDate
+                )
+            );
+        }
     }
 
     return (
@@ -34,8 +49,18 @@ const Settings: React.FC = () => {
                     <Form.Label>Monthly Budget (₹)</Form.Label>
                     <Form.Control type='number' name='budget' value={formData.budget} onChange={handleInputChange} />
                 </Form.Group>
+                <Button variant='primary' className='mt-3 mb-5 d-flex align-items-center' onClick={handleSubmit}>Save</Button>
 
-                <Button variant='primary' className='mt-3 d-flex align-items-center' onClick={handleSubmit}>Save</Button>
+                <h3 className='mb-4'>Clear Data</h3>
+                <Form.Group className='mb-3'>
+                    <Form.Label>Select Range</Form.Label>
+                    <Row>
+                        <Col><Form.Control type='date' value={fromDate} onChange={e => setFromDate(e.target.value)} /></Col>_
+                        <Col><Form.Control type='date' value={toDate} onChange={e => setToDate(e.target.value)} /></Col>
+                    </Row>
+                </Form.Group>
+                <Button variant='danger' className='mt-3 mb-5 d-flex align-items-center' onClick={clearData}>Clear</Button>
+
             </Form>
         </div>
     );
